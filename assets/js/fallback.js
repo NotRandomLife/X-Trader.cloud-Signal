b70dd0e8f915c391234bf6f267eae35a88e719db
NotRandomLife <danicaccialanza@gmail.com>
2025-11-12 14:19:32 +0100

    Delete assets directory

--- assets/js/fallback.js
+++
@@ -1,4 +0,0 @@
-(function(){
-  function apply(){ if(typeof window.__i18nApply==='function'){ window.__i18nApply(); } }
-  if(document.readyState==='loading'){ document.addEventListener('DOMContentLoaded', apply);} else { apply(); }
-})();
