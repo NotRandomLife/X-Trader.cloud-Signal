a710110fa29c0595cb4e1634972a2ef8feab2f62
NotRandomLife <danicaccialanza@gmail.com>
2025-11-12 14:21:18 +0100

    Delete netlify directory

--- netlify/functions/latest.mjs
+++
@@ -1,9 +0,0 @@
-import { getStore } from "@netlify/blobs";
-
-export default async () => {
-  const cors = { "Access-Control-Allow-Origin": "*", "Content-Type": "application/json" };
-  const store = getStore("xtrader-signals");
-  const last = (await store.get("last", { type: "json" })) || null;
-  return new Response(JSON.stringify(last), { status: 200, headers: cors });
-};
-
